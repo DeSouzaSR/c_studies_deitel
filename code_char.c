cec06216f67a909bc0ea1b5d49fb257f05e4d7b6
DeSouzaSR <sandro.fisica@gmail.com>
2023-12-14 16:55:31 -0300

    Add code_char.c

---
+++ code_char.c
@@ -0,0 +1,12 @@
+/*
+Exercise 2.29: Integer value of a character. This program prints the integer
+equivalents of the characters A B C a b c 0 1 2 $ * + / and
+the blank space character.
+*/
+#include <stdio.h>
+int main ( void )
+{
+    printf("\n Character | Code\n");
+    printf("--------------------\n");
+    printf(" A B C a b c 0 1 2 $ * + / | Code\n");
+}
