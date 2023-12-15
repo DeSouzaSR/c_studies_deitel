7dc81e32e257431cf9d7d87c647be4df2adb239f
DeSouzaSR <sandro.fisica@gmail.com>
2023-12-14 21:49:43 -0300

    Finish split_integer.c

--- split_integer.c
+++ split_integer.c
@@ -3,17 +3,35 @@ Split integer
 
 Input: Integer with five digits
 Output: The five digits with black space between them
+
+Note: No array or loops
 */
 #include <stdio.h>
 
 int main ( void )
 {
+    int d1, d2, d3, d4, d5;
     int number;
-    int d1, d2, d3, d4, d5; //Digits
+    int remaining;
+    int digit; //Digits
     
     // Read a integer
     printf("Enter a integer: ");
     scanf("%d", &number);
-    printf("The number entered was: %d", number);
-    // Split 
+    printf("The number entered was %d\n", number);
+
+    // Split
+    d1 = number % 10;
+    remaining = (number - d1)/10;
+    d2 = remaining % 10;
+    remaining = (remaining - d2)/10;
+    d3 = remaining % 10;
+    remaining = (remaining - d3)/10;
+    d4 = remaining % 10;
+    remaining = (remaining - d4)/10;
+    d5 = remaining % 10;
+    remaining = (remaining - d5)/10;
+
+    printf("%d %d %d %d %d\n", d5, d4, d3, d2, d1);
+    
 }
