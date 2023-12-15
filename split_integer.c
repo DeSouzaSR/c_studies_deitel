4a9aaf54375d338aab4258f5a89b29d518e34eea
DeSouzaSR <sandro.fisica@gmail.com>
2023-12-14 21:26:40 -0300

    Create split_integer.c

---
+++ split_integer.c
@@ -0,0 +1,19 @@
+/*
+Split integer
+
+Input: Integer with five digits
+Output: The five digits with black space between them
+*/
+#include <stdio.h>
+
+int main ( void )
+{
+    int number;
+    int d1, d2, d3, d4, d5; //Digits
+    
+    // Read a integer
+    printf("Enter a integer: ");
+    scanf("%d", &number);
+    printf("The number entered was: %d", number);
+    // Split 
+}
