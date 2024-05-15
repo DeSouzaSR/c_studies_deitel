6f1cc823ba1bac54f77f75c67924be7b90255c9b
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2024-05-15 10:06:34 -0300

    Create exercise 3.49.c

---
+++ exercise-3.49-encode.c
@@ -0,0 +1,31 @@
+/*
+Encode and decode a four-digit integer.
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int number = 1111;
+    int d1, d2, d3, d4; // digits
+    int number_coded;
+
+    d4 = number % 10;
+	number = (number / 10);
+	d3 = number % 10;
+	number = number / 10;
+	d2 = number % 10;
+	number = number / 10;
+	d1 = number % 10;
+	number = number / 10;
+
+    d4 = (d4 + 7) % 10;
+    d3 = (d3 + 7) % 10;
+    d2 = (d2 + 7) % 10;
+    d1 = (d1 + 7) % 10;
+
+    number_coded = d3 * 1000 + d4 * 100 + d1 * 10 + d2;
+    
+    printf("Coded number: %d", number_coded);
+    return 0;
+}
