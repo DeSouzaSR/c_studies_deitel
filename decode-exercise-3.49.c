87038b9a159c625c4dbc185531b41a3c90efe092
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2024-05-16 11:28:06 -0300

    Create encode and decode files. Still does not work

--- decode-exercise-3.49.c
+++ decode-exercise-3.49.c
@@ -8,7 +8,7 @@ int main ()
 {
     int number;
     int d1, d2, d3, d4; // digits
-    int number_coded;
+    int number_decoded;
 
 	printf("Enter a number to encode: ");
 	scanf("%d", &number);
@@ -23,15 +23,14 @@ int main ()
 	d1 = number % 10;
 	number = number / 10;
 
-	// Encode
-    d4 = (d4 + 7) % 10;
-    d3 = (d3 + 7) % 10;
-    d2 = (d2 + 7) % 10;
-    d1 = (d1 + 7) % 10;
-
-    number_coded = d3 * 1000 + d4 * 100 + d1 * 10 + d2;
+	d4 = (d2 - 7) % 10;
+	d2 = (d4 - 7) % 10;
+	d3 = (d1 - 7) % 10;
+	d1 = (d3 - 7) % 10;
+	
+	number_decoded = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
     
-    printf("Coded number: %d", number_coded);
+    printf("Coded number: %d", number_decoded);
     
     return 0;
 }
