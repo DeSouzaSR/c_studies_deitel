fdf766e6a9887855d81a0e89ecc1d7a452b2cbd5
DeSouzaSR <sandro.fisica@gmail.com>
2024-12-13 19:00:18 -0300

    I studied chapter 2 again

---
+++ Chapter02/fig02_05.c
@@ -0,0 +1,20 @@
+// Addition program
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int integer1;
+    int integer2;
+    int sum;
+
+    printf("Enter first integer\n");
+    scanf("%d", &integer1);
+
+    printf("Enter second integer\n");
+    scanf("%d", &integer2);
+
+    sum = integer1 + integer2;
+
+    printf("Sum is %d\n", sum);
+    return 0;
+}
